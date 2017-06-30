dad4d90eed0745d85e392038fbb848b7288a9edf
home <fernando.b.cagale@gmail.com>
2017-06-30 13:16:53 -0300

    alter linter

--- service.js
+++ service.js
@@ -5,19 +5,19 @@ require('dotenv').load({silent: true});
 const Seneca = require('seneca');
 
 Seneca({
-    tag: 'base'
-  }).use('consul-registry', {
-    host: process.env.ADDR || '127.0.0.1'
-  }).use('seneca-mesh', {
-    isbase: true,
-    discover: {
-      registry: {
-        active: true
-      },
-      multicast: {
-        active: false
-      }
+  tag: 'base'
+}).use('consul-registry', {
+  host: process.env.ADDR || '127.0.0.1'
+}).use('seneca-mesh', {
+  isbase: true,
+  discover: {
+    registry: {
+      active: true
+    },
+    multicast: {
+      active: false
     }
-  }).ready(function () {
-    console.log('base', this.id);
-  });
+  }
+}).ready(function () {
+  console.log('base', this.id);
+});
